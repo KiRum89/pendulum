a8e9198c67755fcb613b3b9d8e50b41333dfadd7
KirillPlasma <funduk89@gmail.com>
2022-04-19 21:48:51 +0200

    use numpy where possible

--- pendulum_simulation.py
+++ pendulum_simulation.py
@@ -6,7 +6,7 @@ import sys, pygame
 
 m = 1
 L = 0
-r = 100
+l = 100
 g = 5
 dt = 1e-2
 def rhs(r):
@@ -14,48 +14,53 @@ def rhs(r):
     return m*g*x
 
 
-def get_v(L,x,y):
-    vx = L*y/m/r**2
-    vy = -L*x/m/r**2
+def get_v(L,r):
+    x,y = r
+    vx = L*y/m/l**2
+    vy = -L*x/m/l**2
+    return np.array([vx,vy])
 
-    return vx,vy
+def get_accel(v_new,v_old,dt):
+    return (v_new-v_old)/dt
 
-def updateL(Lold,r,vx,vy):
-    
+def updateL(Lold,r):
     return Lold + dt*rhs(r)
 
 
-def step(L,x,y):
-    vx,vy = get_v(L,x,y)
-    x += vx*dt
-    y += vy*dt
-    corr = r/(x**2+y**2)**0.5    
-    x = corr*x
-    y = corr*y
-
-    L = updateL(L,[x,y],vx,vy)
-    return L,x,y
+def step(L,r):
+    v = get_v(L,r)
+    r +=dt*v
+    corr = l/np.sqrt(np.sum(r**2)) 
+    r *=corr
+    L = updateL(L,r)
+    return L,r
  
-size = width,height = 300, 300
-black = (0,0,0)
-linec = pygame.color.Color('white')
-screen=pygame.display.set_mode(size)
 L = 0#150
 theta = np.pi/2-0.01
-x,y = r*np.cos(theta),r*np.sin(theta)
+r = l*np.array([np.cos(theta),np.sin(theta)])
 
-while 1:
-    L,x,y = step(L,x,y)    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT : sys.exit()
-    screen.fill((black))
-    pygame.draw.line(screen,linec,[150,150],[int(x)+150,-int(y)+150],2)
-    pygame.display.flip()
+animate = True
+if animate == True:
+    size = width,height = 300, 300
+    black = (0,0,0)
+    linec = pygame.color.Color('white')
+    screen=pygame.display.set_mode(size)
+
+    while 1:
+        L,r = step(L,r)    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT : sys.exit()
+        screen.fill((black))
+        
+        x,y = r
+        print(x,y)
+        pygame.draw.line(screen,linec,[150,150],[x+150,-y+150],1)
+        pygame.display.flip()
 
 
 
-theta = np.pi/2-0.1
-x,y = r*np.cos(theta),r*np.sin(theta)
+theta = np.pi/3
+r = l*np.array([np.cos(theta),np.sin(theta)])
 vx,vy=0,0
 xx =[]
 yy = []
@@ -66,16 +71,16 @@ LL = []
 
 
 for t in range(0,10000):
-    vx,vy = get_v(L,x,y)
-    x += vx*dt
-    y += vy*dt
-    corr = r/(x**2+y**2)**0.5    
-    x = corr*x
-    y = corr*y
-
-    L = updateL(L,[x,y],vx,vy)
-    xx.append(x)
-    yy.append(y)
-    vvx.append(vx)
-    vvy.append(vy)
+    v = get_v(L,r)
+    
+    r +=dt*v
+    corr = l/np.sqrt(np.sum(r**2)) 
+    r *= corr
+    L = updateL(L,r)
+    xx.append(r[0])
+    yy.append(r[1])
+    vvx.append(v[0])
+    vvy.append(v[1])
     LL.append(L)
+plt.plot(xx)
+plt.show()
